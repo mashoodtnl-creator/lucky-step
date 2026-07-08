a1a5ce641ffa8edef3eaa0bd66eefe3aae89e21b
mashoodtnl-creator <mashoodtnl@gmail.com>
2026-07-09 01:04:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// പേജ് ലോഡ് ചെയ്യുമ്പോൾ സേവ് ചെയ്ത വിവരങ്ങൾ കാണിക്കാൻ
+window.onload = function() {
+    let storedData = localStorage.getItem("chittyData");
+    if (storedData) {
+        document.getElementById("memberTable").innerHTML = storedData;
+    }
+};
+
 function addMember() {
     let name = document.getElementById("name").value;
     let amount = document.getElementById("amount").value;
@@ -8,10 +16,18 @@ function addMember() {
         row.insertCell(0).innerHTML = name;
         row.insertCell(1).innerHTML = amount;
         
-        // ക്ലിയർ ചെയ്യാൻ
+        // വിവരങ്ങൾ localStorage-ൽ സേവ് ചെയ്യാൻ
+        localStorage.setItem("chittyData", table.innerHTML);
+        
         document.getElementById("name").value = "";
         document.getElementById("amount").value = "";
     } else {
         alert("വിവരങ്ങൾ പൂരിപ്പിക്കുക!");
     }
+}
+
+// ഡാറ്റ ക്ലിയർ ചെയ്യാൻ ഒരു ബട്ടൺ വേണമെങ്കിൽ
+function clearData() {
+    localStorage.removeItem("chittyData");
+    location.reload();
 }
